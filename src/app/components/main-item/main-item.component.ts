20c77a02c4ca51bd6ee06a55b5853c0db570ac44
v.nikitin <ipo.it.cx@mail.ru>
2023-06-30 13:54:46 +0500

    Добавлен линтинг + на прекоммите

--- src/app/components/main-item/main-item.component.ts
+++ src/app/components/main-item/main-item.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[app-main-item]',
-  templateUrl: './main-item.component.html',
-  styles: [
-  ]
+	selector: '[app-main-item]',
+	templateUrl: './main-item.component.html',
+	styles: [],
 })
-export class MainItemComponent {
-
-}
+export class MainItemComponent {}
--- src/app/components/main-list/main-list.component.ts
+++ src/app/components/main-list/main-list.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main-list',
-  templateUrl: './main-list.component.html',
-  styles: [
-  ]
+	selector: 'app-main-list',
+	templateUrl: './main-list.component.html',
+	styles: [],
 })
-export class MainListComponent {
-
-}
+export class MainListComponent {}
