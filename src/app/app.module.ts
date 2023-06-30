14bd0201fd862eff3ad8e9a4d8745327afe869a5
v.nikitin <ipo.it.cx@mail.ru>
2023-06-30 13:44:27 +0500

    Добавлен компонент для пункта в общем списке

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainListComponent } from './components/main-list/main-list.component';
+import { MainItemComponent } from './components/main-item/main-item.component';
 
 @NgModule({
-	declarations: [AppComponent, MainListComponent],
+	declarations: [AppComponent, MainListComponent, MainItemComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/components/main-item/main-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-main-item]',
+  templateUrl: './main-item.component.html',
+  styles: [
+  ]
+})
+export class MainItemComponent {
+
+}
