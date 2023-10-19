b0d022ba09488d2dd07ae9825b7c4277c75aa7af
v.nikitin <ipo.it.cx@mail.ru>
2023-10-20 02:21:36 +0500

    Убраны вкладки с итерациями при создании события, поправлены мелочи

--- src/app/components/personal/auth/auth.component.ts
+++ src/app/components/personal/auth/auth.component.ts
@@ -10,10 +10,20 @@ export class AuthComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.form = new FormGroup({
-			login: new FormControl(null, [Validators.required]),
-			password: new FormControl(null, [Validators.required]),
+			email: new FormControl(null, [
+				Validators.required,
+				Validators.email,
+			]),
+			password: new FormControl(null, [
+				Validators.required,
+				Validators.minLength(8),
+			]),
 		});
 	}
 
-	submit() {}
+	submit() {
+		if (this.form.valid) {
+			console.log(this.form.value);
+		}
+	}
 }
