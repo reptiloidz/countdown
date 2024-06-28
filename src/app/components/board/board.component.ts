16e27fb7321bd6facd07eaa3ba410a2d57ddbb5f
v.nikitin <ipo.it.cx@mail.ru>
2024-06-28 17:52:11 +0500

    Анимации табло переписаны на css

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,11 +1,4 @@
 import {
-	animate,
-	state,
-	style,
-	transition,
-	trigger,
-} from '@angular/animations';
-import {
 	Component,
 	HostBinding,
 	Input,
@@ -19,53 +12,6 @@ const ANIMATION_SPEED = 200;
 @Component({
 	selector: 'app-board',
 	templateUrl: './board.component.html',
-	animations: [
-		trigger('halfTop', [
-			state(
-				'void',
-				style({ transform: 'rotateX(0)', boxShadow: 'none' })
-			),
-			state(
-				'*',
-				style({
-					transform:
-						'rotateX(90deg) matrix3d(1, 0, 0, 0, 0, .75, 0, .001, 1, 1, .1, .1, .1, .1, 0, 1)',
-					opacity: 0,
-				})
-			),
-			transition(':enter', [
-				animate(
-					ANIMATION_SPEED + 'ms cubic-bezier(.36,.25,.76,.52)',
-					style({
-						transform:
-							'rotateX(90deg) matrix3d(1, 0, 0, 0, 0, .75, 0, .001, 1, 1, .1, .1, .1, .1, 0, 1)',
-						boxShadow:
-							'var(--board-half-shadow), 0 calc(var(--board-h) / -4) 20px 0 rgba(var(--c-bg-primary), .1)',
-					})
-				),
-			]),
-		]),
-		trigger('halfBottom', [
-			state(
-				'void',
-				style({
-					transform:
-						'rotateX(90deg) matrix3d(1, 0, 0, 0, 0, .75, 0, -.001, 1, 1, .1, .1, .1, .1, 0, 1)',
-					boxShadow:
-						'0 calc(var(--board-h) / 4) 20px 1px rgba(var(--c-bg-primary), .4)',
-				})
-			),
-			transition(':enter', [
-				animate(
-					ANIMATION_SPEED + 'ms cubic-bezier(.36,.25,.76,.52)',
-					style({
-						transform: 'rotateX(0)',
-						boxShadow: 'none',
-					})
-				),
-			]),
-		]),
-	],
 })
 export class BoardComponent implements OnChanges {
 	@HostBinding('class') get componentClass() {
