da4eaa9402bfe801590a9a9bb9d039839c7249fa
v.nikitin <ipo.it.cx@mail.ru>
2025-01-28 13:46:08 +0500

    test(loader): add unit tests for loader component

---
+++ src/app/components/loader/loader.component.test.ts
@@ -0,0 +1,48 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoaderComponent } from './loader.component';
+import { Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+describe('LoaderComponent', () => {
+	let component: LoaderComponent;
+	let fixture: ComponentFixture<LoaderComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [LoaderComponent, MockSvgComponent],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(LoaderComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create the component', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should apply the base class to the host element', () => {
+		const hostElement: HTMLElement = fixture.debugElement.nativeElement;
+		expect(hostElement.className).toContain('loader');
+	});
+
+	it('should pass the iconClass input to the svg element', () => {
+		const testClass = { 'custom-class': true };
+		component.iconClass = testClass;
+		fixture.detectChanges();
+
+		const svgElement = fixture.debugElement.query(By.css('svg'));
+		expect(svgElement.attributes['class']).toContain('custom-class');
+	});
+});
+
+@Component({
+	selector: 'app-svg',
+	template: '<svg></svg>',
+})
+class MockSvgComponent {
+	@Input() name!: string;
+	@Input() class!: string;
+	@Input() ariaHidden!: boolean;
+	@Input() ariaLabel!: string;
+}
