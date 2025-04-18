39afbabacc333e2e69ac1620e0d67477225c7874
v.nikitin <ipo.it.cx@mail.ru>
2025-04-18 21:28:45 +0500

    fix(webpack): update EnvironmentPlugin configuration to use object syntax

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,15 @@ config();
 
 module.exports = {
 	plugins: [
-		new EnvironmentPlugin([
-			'NG_APP_FIREBASE_API_KEY',
-			'NG_APP_FIREBASE_AUTH_DOMAIN',
-			'NG_APP_FIREBASE_DB_URL',
-			'NG_APP_FIREBASE_PROJECT_ID',
-			'NG_APP_FIREBASE_STORAGE_BUCKET',
-			'NG_APP_FIREBASE_APP_ID',
-			'NG_APP_FIREBASE_MEASUREMENT_ID',
-		]),
+		new EnvironmentPlugin({
+			NG_APP_FIREBASE_API_KEY: 'defaultKey',
+			NG_APP_FIREBASE_AUTH_DOMAIN: 'defaultDomain',
+			NG_APP_FIREBASE_DB_URL: 'defaultDbUrl',
+			NG_APP_FIREBASE_PROJECT_ID: 'defaultProjectId',
+			NG_APP_FIREBASE_STORAGE_BUCKET: 'defaultStorageBucket',
+			NG_APP_FIREBASE_MESSAGING_SENDER_ID: 'defaultSenderId',
+			NG_APP_FIREBASE_APP_ID: 'defaultAppId',
+			NG_APP_FIREBASE_MEASUREMENT_ID: 'defaultMeasurementId',
+		}),
 	],
 };
